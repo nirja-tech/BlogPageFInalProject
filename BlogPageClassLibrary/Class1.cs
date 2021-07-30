18bf409baca7b5fc4d4589091aa004333643ca21
Nirja <nirjapathak3@gmail.com>
2021-07-29 17:36:48 -0700

    Created two more empty projects which are BlogPageUnitTest and BloagPageClassLibrary.

---
+++ BlogPageClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlogPageClassLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ BlogPageUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BlogPageUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlogPageUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7bcb87f-e214-481d-a422-f4d4c5b10236")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
