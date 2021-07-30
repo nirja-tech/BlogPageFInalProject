3d45ba557653659703629a0461635946d576644d
Nirja <nirjapathak3@gmail.com>
2021-07-29 20:32:56 -0700

    Added SignIn.cshtml Page

--- BlogPageFInalProject/Controllers/HomeController.cs
+++ BlogPageFInalProject/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace BlogPageFInalProject.Controllers
         {
             return View();
         }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ BlogPageFInalProject/Controllers/SignInController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPageFInalProject.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
